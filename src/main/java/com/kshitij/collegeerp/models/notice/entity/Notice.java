4fb67d3ae97595bbb135c40a9b3d5f27ae885af9
Kshitij Kumar <kshitij.workshop@gmail.com>
2026-06-24 09:24:55 +0530

    table name changed to notices from tables

--- src/main/java/com/kshitij/collegeerp/models/notice/entity/Notice.java
+++ src/main/java/com/kshitij/collegeerp/models/notice/entity/Notice.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "tables")
+@Table(name = "notices")
 @Getter @Setter @Builder
 @AllArgsConstructor
 @NoArgsConstructor
